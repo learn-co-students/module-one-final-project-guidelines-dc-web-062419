6c4a9c06e068eee6fb58af28396aef7ac5983e36
Trevahr Robertson <Trevahr@gmail.com>
2019-07-08 17:13:35 -0400

    starting point, maybe

---
+++ db/migrate/20190708205546_create_questions.rb
@@ -0,0 +1,7 @@
+class CreateQuestions < ActiveRecord::Migration[5.0]
+  def change
+          create_table :questions do |t|
+        t.string :question
+        t.integer :correct_answer
+  end
+end
