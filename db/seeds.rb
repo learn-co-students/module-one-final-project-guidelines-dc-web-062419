b26815c6bbd733a80845bddbe8427d2af972a856
claudiasolbes <cssolbes@hotmail.com>
2019-07-10 16:00:32 -0400

    changed questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,40 +4,90 @@ User.destroy_all
 
 a = [
 {
-category: "General Knowledge",
+category: "Entertainment: Television",
 type: "boolean",
-difficulty: "easy",
-question: "The Great Wall of China is visible from the moon.",
+difficulty: "medium",
+question: "AMC&#039;s &quot;The Walking Dead&quot;, Rick, Carl, Daryl, Morgan, Carol and Maggie were introduced to us in Season 1.",
 correct_answer: "False",
 incorrect_answers: [
 "True"
 ]
 },
 {
-category: "General Knowledge",
+category: "Entertainment: Video Games",
 type: "boolean",
-difficulty: "easy",
-question: "Video streaming website YouTube was purchased in it&#039;s entirety by Facebook for US$1.65 billion in stock.",
-correct_answer: "False",
-incorrect_answers: [
-"True"
-]
-},
-{
-category: "General Knowledge",
-type: "boolean",
-difficulty: "easy",
-question: "French is an official language in Canada.",
+difficulty: "medium",
+question: "Nintendo started out as a playing card manufacturer.",
 correct_answer: "True",
 incorrect_answers: [
 "False"
 ]
 },
 {
-category: "General Knowledge",
+category: "Entertainment: Video Games",
 type: "boolean",
 difficulty: "easy",
-question: "It is automatically considered entrapment in the United States if the police sell you illegal substances without revealing themselves.",
+question: "Peter Molyneux was the founder of Bullfrog Productions.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Science: Computers",
+type: "boolean",
+difficulty: "medium",
+question: "The HTML5 standard was published in 2014.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Cartoon & Animations",
+type: "boolean",
+difficulty: "easy",
+question: "In the &quot;Shrek&quot; film franchise, Donkey is played by Eddie Murphy.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "History",
+type: "boolean",
+difficulty: "easy",
+question: "The Tiananmen Square protests of 1989 were held in Hong Kong.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "Science: Mathematics",
+type: "boolean",
+difficulty: "medium",
+question: "The Pythagorean theorem states that the square of the hypotenuse is equal to the product of the squares of the other two sides.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "Entertainment: Music",
+type: "boolean",
+difficulty: "easy",
+question: "The music video to The Buggle&#039;s &quot;Video Killed the Radio Star&quot; was the first music video to broadcast on MTV.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Film",
+type: "boolean",
+difficulty: "easy",
+question: "The film &quot;2001: A Space Odyssey&quot; was released on December 31st, 2000.",
 correct_answer: "False",
 incorrect_answers: [
 "True"
@@ -46,41 +96,141 @@ incorrect_answers: [
 {
 category: "General Knowledge",
 type: "boolean",
+difficulty: "hard",
+question: "This is the correct spelling of &quot;Supercalifragilisticexpialidocious&quot;.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Science & Nature",
+type: "boolean",
+difficulty: "medium",
+question: "Steel is an alloy of Iron and Carbon.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
 difficulty: "easy",
-question: "Nutella is produced by the German company Ferrero.",
+question: "&quot;Undertale&quot; is an RPG created by Toby Fox and released in 2015.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Music",
+type: "boolean",
+difficulty: "medium",
+question: "Scottish producer Calvin Harris is from the town of Dumfries, Scotland.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Geography",
+type: "boolean",
+difficulty: "easy",
+question: "There is a city called Rome in every continent on Earth.",
 correct_answer: "False",
 incorrect_answers: [
 "True"
 ]
 },
 {
-category: "General Knowledge",
+category: "Science & Nature",
 type: "boolean",
-difficulty: "easy",
-question: "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+difficulty: "medium",
+question: "A defibrillator is used to start up a heartbeat once a heart has stopped beating.",
 correct_answer: "False",
 incorrect_answers: [
 "True"
 ]
 },
 {
-category: "General Knowledge",
+category: "History",
 type: "boolean",
-difficulty: "easy",
-question: "Bulls are attracted to the color red.",
-correct_answer: "False",
+difficulty: "medium",
+question: "Brezhnev was the 5th leader of the USSR.",
+correct_answer: "True",
 incorrect_answers: [
-"True"
+"False"
 ]
 },
 {
-category: "General Knowledge",
+category: "Science & Nature",
+type: "boolean",
+difficulty: "hard",
+question: "It was once believed that injecting shark cartilage into people would prevent them from contracting cancer.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Video Games",
 type: "boolean",
 difficulty: "easy",
-question: "The Sun rises from the North.",
-correct_answer: "False",
+question: "&quot;Half-Life 2&quot; runs on the Source Engine.",
+correct_answer: "True",
 incorrect_answers: [
-"True"
+"False"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
+difficulty: "medium",
+question: "Metal Gear Solid V: The Phantom Pain runs on the Fox Engine.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Sports",
+type: "boolean",
+difficulty: "medium",
+question: "Skateboarding will be included in the 2020 Summer Olympics in Tokyo.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
+difficulty: "easy",
+question: "Tetris is the #1 best-selling video game of all-time.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Mythology",
+type: "boolean",
+difficulty: "easy",
+question: "According to Greek Mythology, Zeus can control lightning.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
+difficulty: "easy",
+question: "In &quot;Super Mario 3D World&quot;, the Double Cherry power-up originated from a developer accidentally making two characters controllable.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
 ]
 },
 {
@@ -94,40 +244,10 @@ incorrect_answers: [
 ]
 },
 {
-category: "General Knowledge",
+category: "Science: Computers",
 type: "boolean",
-difficulty: "easy",
-question: "You can legally drink alcohol while driving in Mississippi.",
-correct_answer: "True",
-incorrect_answers: [
-"False"
-]
-},
-{
-category: "General Knowledge",
-type: "boolean",
-difficulty: "easy",
-question: "On average, at least 1 person is killed by a drunk driver in the United States every hour.",
-correct_answer: "True",
-incorrect_answers: [
-"False"
-]
-},
-{
-category: "General Knowledge",
-type: "boolean",
-difficulty: "easy",
-question: "In 2010, Twitter and the United States Library of Congress partnered together to archive every tweet by American citizens.",
-correct_answer: "True",
-incorrect_answers: [
-"False"
-]
-},
-{
-category: "General Knowledge",
-type: "boolean",
-difficulty: "easy",
-question: "Adolf Hitler was born in Australia. ",
+difficulty: "medium",
+question: "The open source program Redis is a relational database server.",
 correct_answer: "False",
 incorrect_answers: [
 "True"
@@ -136,58 +256,78 @@ incorrect_answers: [
 {
 category: "General Knowledge",
 type: "boolean",
+difficulty: "medium",
+question: "The French word to travel is &quot;Travail&quot;",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "Entertainment: Japanese Anime & Manga",
+type: "boolean",
 difficulty: "easy",
-question: "When you cry in space, your tears stick to your face.",
+question: "In the &quot;Melancholy of Haruhi Suzumiya&quot; series, the narrator goes by the nickname Kyon.",
 correct_answer: "True",
 incorrect_answers: [
 "False"
 ]
 },
 {
-category: "General Knowledge",
+category: "Entertainment: Video Games",
 type: "boolean",
 difficulty: "easy",
-question: "The Lego Group was founded in 1932.",
+question: "Danganronpa 2: Goodbye Despair featured all of the surviving students from the first game.",
 correct_answer: "True",
 incorrect_answers: [
 "False"
 ]
 },
 {
-category: "General Knowledge",
+category: "Entertainment: Japanese Anime & Manga",
 type: "boolean",
 difficulty: "easy",
-question: "One of Donald Trump&#039;s 2016 Presidential Campaign promises was to build a border wall between the United States and Mexico.",
+question: "In Pok&eacute;mon, Ash&#039;s Pikachu refuses to go into a pokeball.",
 correct_answer: "True",
 incorrect_answers: [
 "False"
 ]
 },
 {
-category: "General Knowledge",
+category: "Geography",
 type: "boolean",
-difficulty: "easy",
-question: "Romanian belongs to the Romance language family, shared with French, Spanish, Portuguese and Italian. ",
+difficulty: "medium",
+question: "Liechtenstein does not have an airport.",
 correct_answer: "True",
 incorrect_answers: [
 "False"
 ]
 },
 {
-category: "General Knowledge",
+category: "Geography",
 type: "boolean",
 difficulty: "easy",
-question: "&quot;27 Club&quot; is a term used to refer to a list of famous actors, musicians, and artists who died at the age of 27.",
+question: "California is larger than Japan.",
 correct_answer: "True",
 incorrect_answers: [
 "False"
 ]
 },
 {
-category: "General Knowledge",
+category: "Mythology",
 type: "boolean",
 difficulty: "easy",
-question: "Scotland voted to become an independent country during the referendum from September 2014.",
+question: "According to Greek Mythology, Atlas was an Olympian God.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "Science: Computers",
+type: "boolean",
+difficulty: "medium",
+question: "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
 correct_answer: "False",
 incorrect_answers: [
 "True"
@@ -196,11 +336,171 @@ incorrect_answers: [
 {
 category: "General Knowledge",
 type: "boolean",
-difficulty: "easy",
-question: "The National Animal of Scotland is the Unicorn.",
+difficulty: "medium",
+question: "Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.",
 correct_answer: "True",
 incorrect_answers: [
 "False"
+]
+},
+{
+category: "Entertainment: Music",
+type: "boolean",
+difficulty: "easy",
+question: "Stevie Wonder&#039;s real name is Stevland Hardaway Morris.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
+difficulty: "medium",
+question: "In Monster Hunter Generations, guild style is a type of hunting style.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
+difficulty: "medium",
+question: "In Riot Games &quot;League of Legends&quot; the name of Halloween event is called &quot;The Reckoning&quot;.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "Entertainment: Japanese Anime & Manga",
+type: "boolean",
+difficulty: "easy",
+question: "Kiznaiver is an adaptation of a manga.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "Entertainment: Television",
+type: "boolean",
+difficulty: "hard",
+question: "The Klingon home planet is Qo&#039;noS.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Television",
+type: "boolean",
+difficulty: "medium",
+question: "Klingons once had a period of Democracy in their history, they referred to it as the &quot;Dark Times&quot;.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Science: Computers",
+type: "boolean",
+difficulty: "medium",
+question: "Android versions are named in alphabetical order.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
+difficulty: "medium",
+question: "In &quot;Super Mario World&quot;, the rhino mini-boss, Reznor, is named after the lead singer of the band &quot;Nine Inch Nails&quot;.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "History",
+type: "boolean",
+difficulty: "hard",
+question: "During the Winter War, the amount of Soviet Union soliders that died or went missing was five times more than Finland&#039;s.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Television",
+type: "boolean",
+difficulty: "easy",
+question: "In &quot;Doctor Who&quot;, the Doctor gets his TARDIS by stealing it.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Geography",
+type: "boolean",
+difficulty: "easy",
+question: "Hungary is the only country in the world beginning with H.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
+difficulty: "medium",
+question: "In &quot;League of Legends&quot;, there exists four different types of Dragon.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "Entertainment: Video Games",
+type: "boolean",
+difficulty: "easy",
+question: "The game &quot;Pocket Morty&quot; has a Morty called &quot;Pocket Mortys Morty&quot;?",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Science: Mathematics",
+type: "boolean",
+difficulty: "easy",
+question: "The sum of any two odd integers is odd.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
+]
+},
+{
+category: "General Knowledge",
+type: "boolean",
+difficulty: "hard",
+question: "Spoon theory is a theory, utilizing &quot;Spoons&quot; as a metaphor for energy they can use in a day.",
+correct_answer: "True",
+incorrect_answers: [
+"False"
+]
+},
+{
+category: "Entertainment: Film",
+type: "boolean",
+difficulty: "medium",
+question: "The movie &quot;Tron&quot; received an Oscar nomination for Best Visual Effects.",
+correct_answer: "False",
+incorrect_answers: [
+"True"
 ]
 }
 ]
@@ -211,6 +511,9 @@ a.each do |question|
     Question.find_or_create_by(question: question[:question], correct_answer: question[:correct_answer])
 end
 
-User.find_or_create_by(name: "John")
-User.find_or_create_by(name: "Tim")
-User.find_or_create_by(name: "Jill")
+User.find_or_create_by(name: "John", high_score: 0)
+User.find_or_create_by(name: "Tim", high_score: 0)
+User.find_or_create_by(name: "Jill", high_score: 0)
+User.find_or_create_by(name: "Dan", high_score: 0)
+User.find_or_create_by(name: "Sofia", high_score: 0)
+User.find_or_create_by(name: "Anne", high_score: 0)
